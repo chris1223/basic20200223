e0d5762971263e5020c29299eea919f93371b9b9
chris1223 <chris711223@gmail.com>
2020-02-23 13:24:24 +0800

    Angular 9 實作練習

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TagsComponent } from './tags/tags.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TagsComponent,
+    ArticlesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  keyword = '';
+  invalue = '';
+  isHighLight = false;
+  fontSiz = 12;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  search($event) {
+    this.invalue = this.keyword;
+    this.isHighLight = !this.isHighLight;
+    this.fontSiz = this.fontSiz + 2;
+  }
+}
